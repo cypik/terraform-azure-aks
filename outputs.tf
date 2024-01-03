b7809c8c45fca1790468a9c869605a742b27cd33
cypikarjun <arjun.y@cypik.com>
2024-01-03 16:39:22 +0530

    update output

--- outputs.tf
+++ outputs.tf
@@ -24,29 +24,29 @@ output "node_resource_group" {
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks[*].kube_config
-  sensitive = true
+  value       = azurerm_kubernetes_cluster.aks[*].kube_config
+  sensitive   = true
   description = "Kubeconfig settings for connecting to the Azure Kubernetes Service (AKS) cluster."
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.aks[0].kube_config[0].host
+  value       = azurerm_kubernetes_cluster.aks[0].kube_config[0].host
   description = "The Kubernetes cluster server host."
 
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks[0].kube_config[0].client_certificate
+  value       = azurerm_kubernetes_cluster.aks[0].kube_config[0].client_certificate
   description = "Base64 encoded public certificate used by clients to authenticate to the Kubernetes cluster."
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.aks[0].kube_config[0].client_key
-  description =  "Base64 encoded private key used by clients to authenticate to the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.aks[0].kube_config[0].client_key
+  description = "Base64 encoded private key used by clients to authenticate to the Kubernetes cluster."
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.aks[0].kube_config[0].cluster_ca_certificate
+  value       = azurerm_kubernetes_cluster.aks[0].kube_config[0].cluster_ca_certificate
   description = "Base64 encoded public CA certificate used as the root of trust for the Kubernetes cluster."
 }
 
